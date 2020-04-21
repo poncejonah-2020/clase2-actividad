ebeeecc92afce9981e5fc0233897b26298ed63ad
jonatan.maxi.ponce@gmail.com <jonatan.maxi.ponce@gmail.com>
2020-04-21 20:01:04 -0300

    punto a punto b

---
+++ act_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace actividad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> persona = new Dictionary<string, string>();
+
+            persona.Add("Juan", "55423412");
+            persona.Add("Ernesto", "56985623");
+            persona.Add("Mariana", "54787451");
+
+
+            if (persona.ContainsKey("Juan"))
+                Console.WriteLine(persona["Juan"]);
+            //string nombre = "Pedro";
+            if(persona.TryGetValue("Pedro", out string value))
+                Console.WriteLine(value);
+            else
+                Console.WriteLine("no tiene la llave");
+
+
+            Console.ReadLine();
+            
+            
+
+
+
+        }
+    }
+}
+
