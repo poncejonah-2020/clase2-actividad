0893980e4593e2289b9ddd5ff86feb60b6b2bda7
jonatan.maxi.ponce@gmail.com <jonatan.maxi.ponce@gmail.com>
2020-04-24 20:24:58 -0300

    Contar palabras y eliminar palabras repetidas

---
+++ act_3/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace act_3
+{
+    class Frase
+    {
+        public static void cargaDeListas(string[] text, List<string> palabra, List<string> palabraRem, List<int> contar)
+        {
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                int repeticiones = 0;
+                for (int x = i; x < text.Length; x++)
+                {
+                    if (text[i] == text[x])
+                    {
+                        repeticiones++;
+                    }
+                }
+                palabra.Add(text[i]);
+                contar.Add(repeticiones);
+                if (repeticiones > 1)
+                {
+                    palabraRem.Add(text[i]);
+                }
+
+            }
+        }
+
+        public static void EliminarRepetidos(List<string> palabra, List<string> palabraRem, List<int>contar)
+        {
+            for (int n = 0; n < palabraRem.Count; n++)
+            {
+                Boolean band = true;
+                for (int m = 0; m < palabra.Count; m++)
+                {
+
+                    if (palabra[m] == palabraRem[n])
+                    {
+                        if (band == true)
+                        {
+                            band = false;
+                        }
+                        else
+                        {
+                            palabra.RemoveAt(m);
+                            contar.RemoveAt(m);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        public static void imprimir (List<string> palabra, List<int>contar)
+        {
+            //Mostrar por pantalla el resultado
+            Console.WriteLine("Palabras y sus repeticiones:\n");
+            int cantPalabras = palabra.Count;
+            for (int z = 0; z < palabra.Count; z++)
+            {
+                Console.WriteLine(palabra[z] + ": " + contar[z]);
+            }
+            Console.WriteLine("\n ");
+            Console.WriteLine("la cantidad de palabras es: " + cantPalabras);
+        }
+    }
+}
---
+++ act_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace act_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaro mi variable de almacenamiento de la frase que ingrese el usurario
+            string frase = "";
+            Console.WriteLine("Ingrese la frase: \n");
+
+            //Almaceno la frase ingresada
+            frase = Console.ReadLine();
+
+            //Guardo la frase en un array de String
+            //toUpper: convierte todo a minuscula
+            //Split: delimita la palabra por " "(espacio)
+            string[] texto= frase.ToUpper().Split(" ");
+
+            //Lista final de palabras
+            List<string> palabras = new List<string>();
+
+            //Lista de palabras a eliminar
+            List<string> palabrasRemov = new List<string>();
+
+            //Lista contador de repeticiones
+            List<int> contarPalabras = new List<int>();
+
+            Frase.cargaDeListas(texto, palabras, palabrasRemov, contarPalabras);
+
+            Frase.EliminarRepetidos(palabras, palabrasRemov, contarPalabras);
+
+            Frase.imprimir(palabras, contarPalabras);
+
+
+        }
+    }
+}
